7a111ccee02acd3a4a2cb398cd2ee4f8feac15b6
Zakiev Adel <adel.zakiyev@rtmis.ru>
2022-04-01 00:55:16 +0300

    правка теста

--- src/test/java/test/TestPracticeForm.java
+++ src/test/java/test/TestPracticeForm.java
@@ -42,7 +42,7 @@ public class TestPracticeForm {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+//        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
